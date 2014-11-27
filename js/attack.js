ce75f714408ae3056ad956c1d80ab8273297e82a
Matt Fugere <mjfugere@gmail.com>
2014-11-27 14:18:25 +0000

    Fleshed out Attack class with test attack type

--- js/attack.js
+++ js/attack.js
@@ -1,6 +1,38 @@
-function Attack() {
-    /** TODO: Create this as a singleton with functions available for different attacks.
-     * then add the attack type as a constructor arg to an Enemy instance,
-     * along with an attack function to call their respective attack.
-     **/
-}
+var Attack = (function() {
+    var instance;
+    var attacks;
+    var swoopingDistance;
+
+    function init() {
+        swoopingDistance = { x: 128, y: 256 };
+
+        attacks = {
+            swoop: function(actor, target)
+            {
+                var distance = {};
+                distance.x = Math.abs(actor.sprite.body.position.x - target.sprite.body.position.x);
+                if (!actor.attacking && distance.x < swoopingDistance.x) {
+                    distance.y = target.sprite.body.position.y - actor.sprite.body.position.y;
+                    if (distance.y > 0 && distance.y < swoopingDistance.y) {
+                        actor.sprite.body.gravity.y = -600;
+                        actor.sprite.body.velocity.setTo(actor.startVel.x * 3, 350);
+                        actor.attacking = true;
+                    }
+                }
+            }
+        };
+        return {
+            attack: function(type, actor, target) {
+                attacks[type](actor, target);
+            }
+        };
+    }
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = init();
+            }
+            return instance;
+        }
+    }
+})();
